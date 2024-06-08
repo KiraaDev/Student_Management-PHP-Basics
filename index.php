dde939545bebd2451e043082342fdadb14bf8113
Kira <123292825+KiraaDev@users.noreply.github.com>
2024-06-08 13:48:36 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Php Basics</title>
+    <link rel="stylesheet" href="./public/css/style.css?v=<?php echo time() ?>" />
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <h1>Student Management System</h1>
+			<h3>Welcome here</h3>
+        </header>
+        <section class="login">
+            <a href="./admin/pages/login.php" class="btn">Admin Login</a>
+        </section>
+    </div>
+</body>
+</html>
